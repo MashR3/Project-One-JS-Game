c26c72de41e4a9f206af214683cec86c80ae3e29
MashR3 <michaelash17@googlemail.com>
2017-09-16 22:09:34 +0100

    3. Score counter added

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@ $(function (){
 
 var $alpha = $('#alpha');
 var $beta = $('#beta');
-var pcount = 0;
-var ccount = 0;
+// var pcount = 0;
+// var ccount = 0;
+var $gameScore = 0;
+var level = 0;
+var $scoreDigit = $('#score');
 
 $('#alpha').one('click', function(event){
 	$(this).css({
 		backgroundColor:'#2bce2b'
 	})
-	pcount++;
-	console.log("I have been clicked!");
+	$gameScore = $gameScore + 1;
+	displayScore();
+	// pcount++;
+	console.log($gameScore);
 });
 
 
@@ -19,36 +24,45 @@ $('#beta').one('click', function(event){
 	$(this).css({
 		backgroundColor: 'red'
 	})
-	ccount++;
-	console.log("I have been clicked!");
+	$gameScore = $gameScore - 1
+	displayScore();
+	// ccount++;
+	console.log($gameScore);
 });
 
 
 
-
-
-
-function pProgress () {
-	var $prog = $('#prog')
-	var counter = 5;
-	var track = 25;
-	var id = setInterval(frame, 50);
-
-
-	function frame () {
-		if (track === 750 && counter === 100) {
-			clearInterval(id);
-
-		} else {
-			track += 5;
-			counter += 1;
-			$prog.style.width = track + 'px';
-		}
-	}
+function displayScore () {
+	$scoreDigit.html($gameScore);
 }
 
 
-pProgress();
+// var $mainHeading = $('#main-heading');
+
+// $mainHeading.html('I\'M SO NEW YOU CAN\'T HANDLE ME!!!');
+
+
+// function pProgress () {
+// 	var $prog = $('#prog')
+// 	var counter = 5;
+// 	var track = 25;
+// 	var id = setInterval(frame, 50);
+
+
+// 	function frame () {
+// 		if (track === 750 && counter === 100) {
+// 			clearInterval(id);
+
+// 		} else {
+// 			track += 5;
+// 			counter += 1;
+// 			$prog.style.width = track + 'px';
+// 		}
+// 	}
+// }
+
+
+// pProgress();
 
 
 
