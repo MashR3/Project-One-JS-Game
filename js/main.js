207f0d86aed4f657d33dfc313c0332a927212378
MashR3 <michaelash17@googlemail.com>
2017-09-17 15:02:58 +0100

    Trying to make the progress bar animate on every click of a same-class element

--- js/main.js
+++ js/main.js
@@ -1,45 +1,83 @@
 $(function (){
 
-// var $alpha = $('.correct');
-// var $beta = $('.incorrect');
-// var pcount = 0;
-// var ccount = 0;
 var $gameScore = 0;
 var level = 0;
 var $scoreDigit = $('#score');
+var $prog = $('.prog');
+var $death = $('#board');
 
-$('.correct').one('click', function(event){
+$('.correct').on('click', function(event){
 	$(this).css({
+		borderColor: '#010d01',
 		backgroundColor:'#4ed453'
 	})
+	
+
 	$gameScore = $gameScore + 1;
 	displayScore();
-	// pcount++;
-	console.log($gameScore);
+
+
 });
+
+$('#c1, #c2').on('click', progress());
+
+
+
 
 
 
 $('.incorrect').one('click', function(event){
 	$(this).css({
-		backgroundColor: 'red'
+		borderColor: 'red',
+		backgroundColor: '#c12c2c'
 	})
 	$gameScore = $gameScore - 1;
 	displayScore();
-	// ccount++;
-	console.log($gameScore);
 });
 
 
+	// $smallSquare
+	// 	.fadeOut(200)
+	// 	.delay(50)
+	// 	.fadeIn(100)
+	// 	.setInterval(50);
+
+
+$('.neutral').one('click', function(event){
+	$(this).css({
+		borderColor: '#66480a',
+		backgroundColor: '#e9bc80'
+	})
+})
+
+
+$('.death').one('click', function(event){
+	// $death === .backgroundColor 'red'
+
+	$(this).css({
+		borderColor: 'white',
+		backgroundColor: 'black',
+		color: '#ce9d43'
+		// checkDeath();
+	})
+})
 
 function displayScore () {
 	$scoreDigit.html($gameScore);
 }
 
 
+function checkDeath () {
+	clearInterval($death) 
 
+}
 
-
+function progress() {
+	// $('.correct').onclick(function(event){
+		$prog === width ==='+100px',
+	
+	// })
+}
 
 
 // MOVING BACKGROUND
